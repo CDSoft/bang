ab15c9f4686392aa246f495299d06680bc3a86ef
Christophe Delord <cdelord.fr>
2023-12-13 21:01:14 +0100

    stress test update

--- test/stress-gen.lua
+++ test/stress-gen.lua
@@ -9,6 +9,9 @@ local Nlibs = 100
 local main = assert(arg[1], "Missing argument")
 local root = fs.dirname(main)
 
+local nb_sources = 0
+local nb_dirs = 0
+
 local function gen(base, level)
     if level == 0 then
         for i = 1, N do
@@ -17,11 +20,13 @@ local function gen(base, level)
             local k = "k_" .. c : sub(#root+2, -1) : splitext() : gsub("[/.-]+", "_")
             assert(fs.write(h, "extern const int "..k..";\n"))
             assert(fs.write(c, "#include \""..h:basename().."\"\nconst int "..k.." = 42;\n"))
+            nb_sources = nb_sources + 1
         end
     else
         for i = 1, N do
             local d = base/tostring(i)
             fs.mkdir(d)
+            nb_dirs = nb_dirs + 1
             gen(d, level-1)
         end
     end
@@ -29,6 +34,7 @@ end
 
 for i = 1, Nlibs do
     fs.mkdir(root/"lib-"..tostring(i))
+    nb_dirs = nb_dirs + 1
     gen(root/"lib-"..tostring(i), depth)
 end
 
@@ -37,3 +43,7 @@ int main(void) {
     return 0;
 }
 ]]))
+nb_sources = nb_sources + 1
+
+print("number of files      :", nb_sources)
+print("number of directories:", nb_dirs)
--- test/stress.lua
+++ test/stress.lua
@@ -5,28 +5,41 @@ local fs = require "fs"
 local root = arg[1]
 assert(fs.is_dir(root))
 
-var "builddir" (root)
+var "builddir" (root/".build")
 
 rule "cc" { description = "CC $in",  command = "cc -MD -MF $depfile -c $in -o $out", depfile = "$out.d" }
 rule "ar" { description = "AR $out", command = "ar -crs $out $in" }
 rule "ld" { description = "LD $out", command = "cc $in -o $out" }
 
-build(root/"stress") { "ld",
-
-    build(root/"main.o") { "cc", root/"main.c" },
-
-    fs.dir(root)
-    : filter(function(lib) return fs.is_dir(root/lib) end)
-    : map(function(lib)
-        return build(root/lib/lib..".a") { "ar",
-            ls (root/lib/"**.c")
-            : map(function(src)
-                local obj = src:splitext()..".o"
-                return build(obj) { "cc", src }
-            end)
-        }
-    end),
-
+rule "clangtidy" {
+    description = "CK $in",
+    command = "clang-tidy --quiet --warnings-as-errors=* $in > $out 2>/dev/null",
 }
 
-default(root/"stress")
+default {
+    build("$builddir/stress") { "ld",
+
+        build("$builddir/main.o") { "cc", root/"main.c",
+            validations = {
+                build("$builddir/main.ok") { "clangtidy", root/"main.c" },
+            }
+        },
+
+        ls(root/"*")
+        : filter(function(lib) return fs.is_dir(lib) end)
+        : map(function(lib)
+            return build("$builddir"/lib..".a") { "ar",
+                ls (lib/"**.c")
+                : map(function(src)
+                    local obj = "$builddir"/src:splitext()..".o"
+                    return build(obj) { "cc", src,
+                        validations = {
+                            build(obj:splitext()..".ok") { "clangtidy", src },
+                        }
+                    }
+                end)
+            }
+        end),
+
+    }
+}
